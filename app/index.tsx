8c98bc9e7be3734e41901b4b8357bc03e7dfe63a
Felix <gatoab2@gmail.com>
2025-08-19 17:34:11 -0500

    TailwindCSS add

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,22 @@
 import { Text, View } from "react-native";
-import "./global.css"; // Import global styles
 
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum
+        id nemo, nulla ea dicta quasi similique reiciendis iste iure sint
+        molestiae ab, nisi vel eum corrupti? Asperiores, cupiditate delectus.!
+      </Text>
+      <Text className="text-xl font-bold text-blue-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum
+        id nemo, nulla ea dicta quasi similique reiciendis iste iure sint
+        molestiae ab, nisi vel eum corrupti? Asperiores, cupiditate delectus.!
+      </Text>
+      <Text className="text-xl font-bold text-blue-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum
+        id nemo, nulla ea dicta quasi similique reiciendis iste iure sint
+        molestiae ab, nisi vel eum corrupti? Asperiores, cupiditate delectus.!
       </Text>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: [
-    "./App.tsx",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
     "./presentation/**/*.{js,jsx,ts,tsx}",
