d520d70924b6edbd77683f64d2e859da8b5cbf04
Felix <gatoab2@gmail.com>
2025-08-19 12:17:03 -0500

    feat: delete files

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import "./global.css"; // Import global styles
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
